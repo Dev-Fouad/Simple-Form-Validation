338d4cffcba541b34fdf3287c66bbaec46205005
fouad ajayi <fouadajayi6@gmail.com>
2022-11-03 07:52:33 +0100

    Made new changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let form = document.querySelector('.signup-form')
-let feedback = document.querySelector('.feedback')
+let Feedback = document.querySelector('.feedback')
 
 
 form.addEventListener('submit' , function(e){
@@ -7,23 +7,25 @@ form.addEventListener('submit' , function(e){
     e.preventDefault()
 
     //Basic validation
-    let username = form.username.value
+    let username = form.Username.value
     let Usernamepattern = /^[a-zA-Z]{6,12}$/;
 
 
     if(Usernamepattern.test(username)){
-        feedback.textContent = "You are valid"
-    }else 
+        Feedback.textContent = "You are valid"
+    }else{
+        Feedback.textContent = "Enter a valid Username"
+    }
 })
 
 //Testing Regex
-const username = 'shaunnn';
-const pattern = /^[a-z]{6,}$/;
+// const username = 'shaunnn';
+// const pattern = /^[a-z]{6,}$/;
 
-let result = pattern.test(username)
+// let result = pattern.test(username)
 
-if (result){
-    console.log('You are valid')
-}else {
-    console.log("You are invalid");
-}
+// if (result){
+//     console.log('You are valid')
+// }else {
+//     console.log("You are invalid");
+// }
